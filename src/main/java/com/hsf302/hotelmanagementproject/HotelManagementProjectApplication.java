b8d6ad2d890a0439dcaeec362482fc9471dd832c
Khoa elder <vokhoa2004vg@gmail.com>
2026-01-24 10:56:58 +0700

    First

---
+++ src/main/java/com/hsf302/hotelmanagementproject/HotelManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hsf302.hotelmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagementProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsf302/hotelmanagementproject/HotelManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsf302.hotelmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
