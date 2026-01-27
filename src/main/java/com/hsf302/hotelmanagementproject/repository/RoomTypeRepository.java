b319f3d12914f657f7861f35799a464036c75cab
tri2k5 <trilttlk12@gmail.com>
2026-01-27 07:25:20 +0700

    hĩed home search

--- src/main/java/com/hsf302/hotelmanagementproject/repository/RoomTypeRepository.java
+++ src/main/java/com/hsf302/hotelmanagementproject/repository/RoomTypeRepository.java
@@ -1,4 +1,47 @@
 package com.hsf302.hotelmanagementproject.repository;
 
-public interface RoomTypeRepository {
+import com.hsf302.hotelmanagementproject.entity.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public interface RoomTypeRepository extends JpaRepository<RoomType, Long> {
+
+    @Query("""
+        SELECT rt, COUNT(r.roomId) - COUNT(b.bookingId)
+        FROM RoomType rt
+        JOIN rt.hotel h
+        JOIN Room r ON r.roomType = rt
+        LEFT JOIN Booking b
+            ON b.room = r
+            AND b.status IN ('CONFIRMED', 'CHECKED_IN')
+            AND NOT (b.checkoutDate <= :checkin OR b.checkinDate >= :checkout)
+        WHERE h.hotelId = :hotelId
+        GROUP BY rt
+        HAVING COUNT(r.roomId) - COUNT(b.bookingId) > 0
+    """)
+    List<Object[]> findAvailableRoomTypesByHotel(
+            @Param("hotelId") Long hotelId,
+            @Param("checkin") LocalDateTime checkin,
+            @Param("checkout") LocalDateTime checkout
+    );
+    @Query("""
+    SELECT COUNT(r.roomId) - COUNT(b.bookingId)
+    FROM Room r
+    LEFT JOIN Booking b
+        ON b.room = r
+        AND b.status IN ('CONFIRMED', 'CHECKED_IN')
+        AND NOT (b.checkoutDate <= :checkin OR b.checkinDate >= :checkout)
+    WHERE r.roomType.roomTypeId = :roomTypeId
+""")
+    int countAvailableRooms(
+            @Param("roomTypeId") Long roomTypeId,
+            @Param("checkin") LocalDateTime checkin,
+            @Param("checkout") LocalDateTime checkout
+    );
+
 }
--- src/main/java/com/hsf302/hotelmanagementproject/service/SearchServiceImpl.java
+++ src/main/java/com/hsf302/hotelmanagementproject/service/SearchServiceImpl.java
@@ -1,4 +1,59 @@
 package com.hsf302.hotelmanagementproject.service;
 
-public class SearchServiceImpl {
+import com.hsf302.hotelmanagementproject.entity.Hotel;
+import com.hsf302.hotelmanagementproject.entity.RoomType;
+import com.hsf302.hotelmanagementproject.repository.HotelRepository;
+import com.hsf302.hotelmanagementproject.repository.RoomTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SearchServiceImpl implements SearchService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    private RoomTypeRepository roomTypeRepository;
+
+    @Override
+    public Map<Hotel, List<Object[]>>  searchAvailableRooms(
+            LocalDateTime checkin,
+            LocalDateTime checkout
+    ) {
+        Map<Hotel, List<Object[]>> result = new LinkedHashMap<>();
+
+        List<Hotel> hotels = hotelRepository.findAll();
+
+        for (Hotel hotel : hotels) {
+            List<Object[]> roomTypes =
+                    roomTypeRepository.findAvailableRoomTypesByHotel(
+                            hotel.getHotelId(), checkin, checkout);
+
+            if (!roomTypes.isEmpty()) {
+                result.put(hotel, roomTypes);
+            }
+        }
+        return result;
+    }
+    @Override
+    public int countAvailableRooms(
+            Long roomTypeId,
+            LocalDateTime checkin,
+            LocalDateTime checkout
+    ) {
+        return roomTypeRepository.countAvailableRooms(
+                roomTypeId, checkin, checkout
+        );
+    }
+
+    @Override
+    public RoomType getRoomType(Long id) {
+        return roomTypeRepository.findById(id).orElse(null);
+    }
 }
