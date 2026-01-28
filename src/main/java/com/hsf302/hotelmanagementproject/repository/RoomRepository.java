be546cd341a6fd6359018583b0b71ce5cc12ab43
Khoa elder <vokhoa2004vg@gmail.com>
2026-01-28 09:50:34 +0700

    add missing repo files

--- src/main/java/com/hsf302/hotelmanagementproject/repository/RoomRepository.java
+++ src/main/java/com/hsf302/hotelmanagementproject/repository/RoomRepository.java
@@ -1,7 +1,24 @@
 package com.hsf302.hotelmanagementproject.repository;
 
 import com.hsf302.hotelmanagementproject.entity.Room;
+import com.hsf302.hotelmanagementproject.entity.enums.RoomStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("""
+    SELECT r FROM Room r
+    WHERE r.roomType.roomTypeId = :typeId
+      AND r.status = com.hsf302.hotelmanagementproject.entity.enums.RoomStatus.AVAILABLE
+""")
+    List<Room> findAvailableByRoomType(@Param("typeId") Long typeId);
+
+    long countByRoomType_RoomTypeId(Long roomTypeId);
+    List<Room> findByRoomType_RoomTypeIdAndStatus(Long roomTypeId, RoomStatus status);
+
 }
